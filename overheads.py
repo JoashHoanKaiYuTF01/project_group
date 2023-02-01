e9fdb5bc56f738508340e150813c045f7fcd3f18
JoashHoanKaiYuTF01 <s10239696@connect.np.edu.sg>
2023-02-01 15:12:08 +0800

    used proper, clean way of enumerating

--- overheads.py
+++ overheads.py
@@ -1,10 +1,8 @@
-from pathlib import Path  
-import csv    #import file 
+from pathlib import Path
+import csv
 
-#create function
 def overheads():
     global categories, values
-    #create empty list to append values inside
     categories = []
     values = []
     current_dir = Path.cwd()/"Overheads.csv" 
@@ -20,15 +18,27 @@ def overheads():
             values.append(float(i[1]))
         
         max_value = max(values)
-        index_of_max_value = [index for index, item in enumerate(values) if item == max_value][0]
+        #index_of_max_value = [index for index, item in enumerate(values) if item == max_value][0]
         
+        for index, item in enumerate(values):
+            if item == max_value:
+                index_of_max_value = index
+
         highest_overhead = categories[index_of_max_value][0]
         highest_overhead_value = float(categories[index_of_max_value][1])
         
         return f"Highest Overhead Category: {highest_overhead}, Amount: {highest_overhead_value}"
         
 print(overheads())
-#execute function 
+
+
+
+    
+
+            
+
+        
+        
 
 
 
