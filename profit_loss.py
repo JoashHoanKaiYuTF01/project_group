112f96df6506ffdce10d5e4bc200cff1c97cb62c
TamChiYaoTF01 <123933563+TamChiYaoTF01@users.noreply.github.com>
2023-02-01 14:44:33 +0800

    made changes to the print statement

--- profit_loss.py
+++ profit_loss.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import csv
 
 #creates function
-def profit_and_loss(): ##
+def profit_and_loss():
     """
     -
     """
@@ -51,7 +51,7 @@ print(cwd)
 file_path = cwd/"summary_report.txt"
 file_path.touch()
 print(file_path)
-print(file_path.exists())
+print(file_path.exists()) 
 
 
 
