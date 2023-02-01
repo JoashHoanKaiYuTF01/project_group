9d045a7e99aa030edef3fe7892170c51a3195da2
JoashHoanKaiYuTF01 <s10239696@connect.np.edu.sg>
2023-02-01 15:48:01 +0800

    improved elegance of profit_loss function

--- profit_loss.py
+++ profit_loss.py
@@ -14,6 +14,7 @@ def profit_and_loss():
     # create a file path using current working directory to link to csv file
     file_path = Path.cwd()/"Profits and Loss.csv" 
     
+    
     with file_path.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
         next(reader) # skip header
@@ -21,39 +22,28 @@ def profit_and_loss():
          # append days and corresponding profits to empty list profit = [] 
         for row in reader:
             profit.append([row[0], row[4]])
-        
-        # access individual transactions in each row of the table 
-        for i in range(len(profit)): # for each row in the table
+            
+        prev_figure = profit[0][1]
 
-            # append profit and profit change for each day to empty list
-            values.append([profit[i][0], (float(profit[i][1]) - float(profit[i-1][1]))])
-        
-        # remove the profit difference for Day 30
-        values[0][-1] = 0
-      
-        #deficits = [i for i in values if i[1]<0]
-        #print(deficits)
-        for deficits in values:
-            print(values)
-            if deficits[1] < 0: # i[1] is the change in profit, the second column in the table
-                
-                if len(deficits) > 0:
-                    # ensure that function will return multiple days of profit deficit if applicable
-                    for i in range(len(deficits)):
-                         print(f"[Profit Deficit] Day: {deficits[0]}, Amount: {deficits[1]}")
-                else:
-                    print("Profit increase! well done!")
-# execute function
+        counter = 0
+
+        for value in profit:
+
+            profit_difference = float(value[1]) - float(prev_figure[1])
+
+            if profit_difference < 0:
+
+                print(f"[Profit Deficit] Day: {value[0]}  Amount: {profit_difference}")
+
+
+            else:
+                counter +=1       
+                if counter == 11: 
+                        return f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+            prev_figure = value  
+
+
 profit_and_loss()
-
-cwd = Path.cwd()
-print(cwd)
-file_path = cwd/"summary_report.txt"
-file_path.touch()
-print(file_path)
-print(file_path.exists()) 
-
-
 
 
 
