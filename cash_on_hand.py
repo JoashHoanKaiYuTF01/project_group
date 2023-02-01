72caf602b2d903aa0d0cd0cae63eb3c6966c59aa
JoashHoanKaiYuTF01 <s10239696@connect.np.edu.sg>
2023-02-01 15:24:49 +0800

    redid cash_on_hand.py from backup

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,10 +1,10 @@
 from pathlib import Path   #imports path 
-import csv    #imports file 
+import csv
 
 #creates a function 
 def COH(): 
     # create empty lists to append cash on hand and corresponding days from csv
-    global COH_values, cash_on_hand, deficits
+    global cash_on_hand
     cash_on_hand = []
     
     # create a file path using current working directory to link to csv file
@@ -24,61 +24,15 @@ def COH():
         prev_figure = cash_on_hand[0][1]
 
         counter = 0
-        prev_figure = cash_on_hand[counter][1]
-
-        #print(cash_on_hand)
-        flag_list = []
+        
         for value in cash_on_hand:
                 
                 cash_difference = float(value[1])-float(prev_figure[1])
                 #print(cash_difference)
 
                 if cash_difference < 0:
-                    flag_list.append(cash_difference)
-            
-                counter += 1
-                    
-                #print(flag_list)   
-            #elif cash_difference >= 0:
-                #print("cash surplus")
-        
-        
-        #print(cash_on_hand)
-        #print(cash_on_hand[0][1])
-        #print(value)
-        #print(value[1])
-        print(flag_list)
-
-
-
-
-
-
-        #deficits = 0
-        #day = 0
-
-        #list1 = []
-        #for i in COH_values:
-            #if i[1]<0:
-                #day = i[0]
-                #deficits = i[1]
-                
-                #day.append(i[0])
-                #deficits.append(i[1])
-        #print(deficits)
-        #print(day)
-                #list1 = list1.append(f"[Cash on Hand Deficit] Day: {day}, Amount: {deficits}")
-                #return list1
-          
-            
-
-        #print(COH_values)
-        #print(cash_on_hand)
-
-        for deficits in COH_values:
-            
-                #print(deficits)
-                if deficits[1] < 0: # i[1] is the change in profit, the second column in the table
+                    #flag_list.append(cash_difference)
+                    print(f"[Cash Deficit] Day: {value[0]}  Amount: {cash_difference}")
                 
                 else:
                     counter +=1       
@@ -86,7 +40,7 @@ def COH():
                         return f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
                 prev_figure = value  
              
-#executes function
+#executes function
 COH()
 
 
