3b2b53e18bc2b2facc32adf1eeeceb37702296c6
JoashHoanKaiYuTF01 <s10239696@connect.np.edu.sg>
2023-02-02 17:20:51 +0800

    added comments

--- overheads.py
+++ overheads.py
@@ -1,27 +1,39 @@
 from pathlib import Path  
 import csv    #import file 
 
-
-file_path= Path.cwd()/"Overheads.csv" 
+# create file path to csv report
+file_path= Path.cwd()/"csv_reports"/"Overheads.csv" 
 
 with file_path.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
+
+    # create empty lists to store overhead transactions
     categories = []
     values = []
+
     for row in reader:
+            # append overhead transactions to empty list
             categories.append(row)
 #create function
 def overheads():
-        fp = Path.cwd()/"summary_report.txt"
-        with fp.open(mode = "w") as file:
+    """
+    insert docstrings 
+    """
+
+    # create file path to summary_report.txt
+    fp = Path.cwd()/"summary_report.txt"
+    with fp.open(mode = "w") as file:
             global categories, values
-        
+
+            # store overhead values in empty list
             for i in categories:
                 values.append(float(i[1]))
             
+            # identify highest overhead value
             max_value = max(values)
-        
+
+            # initialise variable
             maxindex = 0
             for index, item in enumerate(values):
                     list2 = []
@@ -29,14 +41,14 @@ def overheads():
                         maxindex = index
                 
             highest_overhead = categories[maxindex][0]
-            #print(categories[index_of_max_value][0])
+            
             highest_overhead_value = float(categories[maxindex][1])
                 
-            list2 = [f"Highest Overhead Category: {highest_overhead}, Amount: {highest_overhead_value}"]
+            list2 = [f"Highest Overhead Category: {highest_overhead}, Amount: {highest_overhead_value}%"]
                 
             for i in list2:
                 file.writelines(f"{i}\n")
-#overheads()
+
 
 
 
--- profit_loss.py
+++ profit_loss.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 import csv   #import file 
 
-file_path = Path.cwd()/"Profits and Loss.csv"
+# create file_path to csv files
+file_path = Path.cwd()/"csv_reports"/"Profits and Loss.csv"
 
 with file_path.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
     profit = []
     values = []
+
+    # filter out only day and profit values
     for row in reader:
         profit.append([row[0], row[4]])
 
@@ -17,22 +20,31 @@ def profit_and_loss():
     """
     -    
     """
+
+    # create file path to summary_report.txt
     fp = Path.cwd()/"summary_report.txt"
     with fp.open(mode = "a") as file:
+        
         counter = 0
         global profit, values, deficits
-            
+
+        # assign first profit figure to a variable    
         prev_figure = profit[0][1]
 
-        #counter = 0
+        
 
         for value in profit:
 
+            # equation to find profit difference between different days
             profit_difference = float(value[1]) - float(prev_figure[1])
-            list1 = []
-            if profit_difference < 0:
 
-                list1.append(f"[Profit Deficit] Day: {value[0]}  Amount: {profit_difference}")
+            # empty list to append profit deficit day and corresponding amount
+            list1 = []
+
+            if profit_difference < 0:
+                
+                # append profit deficit day and corresponding amount to empty list
+                list1.append(f"[Profit Deficit] Day: {value[0]}  Amount: HKD{abs(profit_difference)}")
 
             else:
                 counter +=1       
@@ -40,12 +52,13 @@ def profit_and_loss():
                         
                     list1 = ["[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"]
                     return(list1)
+            # to ensure that profit value iterates through the list of profit values   
             prev_figure = value  
             
+            # write profit deficits to summary_report.txt
             for i in list1:
                 file.writelines(f"{i}\n")
 
-#profit_and_loss()
 
 
 
